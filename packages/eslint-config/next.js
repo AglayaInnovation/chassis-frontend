5f527692f2dce43e8a7d2d7652db00e9df317ea6
Jose A. Gonzalez <jglez.dev@gmail.com>
2024-07-05 20:18:49 -0500

    chore: Add new rules nextjs
    - Add new rule for file next js

--- packages/eslint-config/next.js
+++ packages/eslint-config/next.js
@@ -30,5 +30,47 @@ module.exports = {
     ".*.js",
     "node_modules/",
   ],
+  rules: {
+    "keyword-spacing": 2,
+    "space-before-blocks": "error",
+    "object-curly-spacing": ["error", "always"],
+    "array-bracket-spacing": ["error", "always"],
+    "block-spacing": ["error", "always"],
+    "object-property-newline": [
+      "error",
+      {
+        allowAllPropertiesOnSameLine: false,
+      },
+    ],
+    "no-console": "error",
+    "no-unused-vars": "warn",
+    indent: ["error", 4],
+    "linebreak-style": ["error", "unix"],
+    quotes: ["error", "double"],
+    semi: ["error", "always"],
+    "no-cond-assign": ["error", "always"],
+    "import/order": [
+      "error",
+      {
+        groups: [
+          "builtin",
+          "external",
+          "internal",
+          "parent",
+          "sibling",
+          "index",
+        ],
+        "newlines-between": "always",
+      },
+    ],
+    "no-multiple-empty-lines": [
+      "error",
+      {
+        max: 1,
+        maxEOF: 0,
+      },
+    ],
+    curly: "error",
+  },
   overrides: [{ files: ["*.js?(x)", "*.ts?(x)"] }],
 };
